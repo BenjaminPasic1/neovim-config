a153baaaf108e69f46dd67c8974e4d4d6060f4e5
Benjamin Pasic <benjamin.pasic1@gmail.com>
2025-09-02 19:32:57 +0200

    few options and binds

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,4 +1,11 @@
-vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>w", ":w<CR>")
 vim.keymap.set("n", "<leader>q", ":q<CR>")
+
+-- Center after moving half-page
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { silent = true })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { silent = true })
+
+-- Center after searching
+vim.keymap.set('n', 'n', 'nzzzv', { silent = true })
+vim.keymap.set('n', 'N', 'Nzzzv', { silent = true })
