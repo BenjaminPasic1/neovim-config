61b6d0d41042d940e8151eb3e0d5deb61a639617
Benjamin Pasic <benjamin.pasic1@gmail.com>
2025-09-02 19:05:53 +0200

    base config

---
+++ lua/plugins/colors.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin", -- optional but nice for clarity
+    lazy = false,        -- load immediately
+    priority = 1000,     -- make sure it loads before other plugins
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha", -- latte, frappe, macchiato, mocha
+        transparent_background = false,
+        integrations = {
+          treesitter = true,
+          nvimtree = true,
+          telescope = true,
+        },
+      })
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+}
+
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,24 @@
+require("lazy").setup({
+    { "nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate" }
+})
+
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        local configs = require("nvim-treesitter.configs")
+        configs.setup({
+            highlight = {
+                enable = true,
+            },
+            indent = { enable = true },
+            autotag = { enable = true },
+            ensure_installed = {
+                "c",
+                "lua",
+                "python"
+            },
+            auto_install = false,
+        })
+    end
+}
