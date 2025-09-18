2b5ed89039930e2810d8cf538ea5cc6d79d5dddf
Benjamin Pasic <benjamin.pasic1@gmail.com>
2025-09-19 01:23:38 +0200

    switched goyo with zenmode to center screen when on larger monitors

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -31,7 +31,7 @@ return {
     local harpoon = require('harpoon')
     vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
     vim.keymap.set("n", "<leader>hm", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-    vim.keymap.set("n", "<leader>fl", function() toggle_telescope(harpoon:list()) end,
+    vim.keymap.set("n", "<leader>ht", function() toggle_telescope(harpoon:list()) end,
       { desc = "Open harpoon window" })
     vim.keymap.set("n", "<leader>hc", function() harpoon:list():clear() end)
     vim.keymap.set("n", "<C-p>", function() harpoon:list():prev() end)
