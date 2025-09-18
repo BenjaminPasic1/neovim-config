d4b82349d2618ec3f87d0c775aa88fcf081b8c7a
Benjamin Pasic <benjamin.pasic1@gmail.com>
2025-09-18 22:14:33 +0200

    switched to nvim's lsp, so no package manager or blink.cmp

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -22,20 +22,11 @@ vim.keymap.set("n", "<leader>th", ":split | terminal<CR>", { silent = true })
 -- Vertical terminal
 vim.keymap.set("n", "<leader>tv", ":vsplit | terminal<CR>", { silent = true })
 
--- Move between windows using Ctrl + h/j/k/l
-vim.keymap.set("n", "<C-h>", "<C-w>h", { silent = true })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { silent = true })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { silent = true })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { silent = true })
+-- Toggle between two windows
+vim.keymap.set("n", "<C-h>", "<C-w>w", { silent = true })
 
 -- Close current window
 vim.keymap.set("n", "<C-c>", ":close<CR>", { silent = true })
-
--- Terminal mode window navigation
-vim.keymap.set("t", "<C-h>", "<C-\\><C-n><C-w>h", { silent = true })
-vim.keymap.set("t", "<C-j>", "<C-\\><C-n><C-w>j", { silent = true })
-vim.keymap.set("t", "<C-k>", "<C-\\><C-n><C-w>k", { silent = true })
-vim.keymap.set("t", "<C-l>", "<C-\\><C-n><C-w>l", { silent = true })
 
 -- In terminal mode, Ctrl-c closes the terminal window
 vim.keymap.set("t", "<C-c>", "<C-\\><C-n>:close<CR>", { silent = true })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,23 @@
 vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.relativenumber = true
-vim.opt.shiftwidth = 4
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+
+vim.opt.smartcase = true
 vim.opt.ignorecase = true
+
+vim.opt.signcolumn = 'yes'
+vim.opt.cursorline = true
+
+vim.opt.inccommand = 'split'
+
+vim.opt.wrap = true
+vim.opt.breakindent = true
+
+vim.opt.winborder = 'rounded'
+
 vim.opt.clipboard:append("unnamedplus")
